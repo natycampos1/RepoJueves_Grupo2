c713ef1e46c49ed6a17e46a259a8f79e0b606923
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-06-03 16:38:29 -0600

    Paginas completas
    Todas navegan correctamente

--- PJ_GRUPODOS/Controllers/HomeController.cs
+++ PJ_GRUPODOS/Controllers/HomeController.cs
@@ -1,20 +1,58 @@
+using PJ_GRUPODOS.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace PJ_GRUPODOS.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Index(string CorreoElectronico, string Contrasenna)
+        {
+            return RedirectToAction("Principal", "Home");
+        }
+
+        #region Registrar
+
+        [HttpGet]
+        public IActionResult Registrar()
         {
             return View();
         }
 
-      
+        [HttpPost]
+        public IActionResult Registrar(UsuarioModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        #endregion
+
+        #region RecuperarAcceso
+
+        [HttpGet]
+        public IActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RecuperarAcceso(string CorreoElectronico)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        #endregion
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+
     }
 }
---
+++ PJ_GRUPODOS/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PJ_GRUPODOS.Models
+{
+    public class UsuarioModel
+    {
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Contrasenna { get; set; } = string.Empty;
+    }
+}
+
