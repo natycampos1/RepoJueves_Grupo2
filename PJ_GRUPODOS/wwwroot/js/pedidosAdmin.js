9aedd1bf3ddc15fb51359ccbd9e2adc6c5e85830
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-08-17 16:29:37 -0600

    listo
    dataform

---
+++ PJ_GRUPODOS/wwwroot/js/pedidosAdmin.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    new DataTable('#tblPedidos', {
+        responsive: true,
+        pageLength: 10,
+        order: [],
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/2.3.4/i18n/es-ES.json'
+        }
+    });
+});
